bb15b0736d643beb362d98fdd72961583045472d
Egor Nedelko <gojiramedia@gmail.com>
2020-10-07 12:41:40 +0300

    feat: add start and end of string comparison logic, results: 12/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-    //bracketsConfig = /\)$/
+    let round = /\)$/
+    let square = /\]$/
+    let curly = /\}$/
+    let straight = /\|$/
+
+    let beg = str.slice(0, 1)
+
+    switch (beg) {
+        case '(':
+            return str.endsWith(')') ? true : false
+        case '[':
+            return str.endsWith(']') ? true : false
+        case '{':
+            return str.endsWith('}') ? true : false
+        case '|':
+            return str.endsWith('|') ? true : false
+    }
+    //return
 }
