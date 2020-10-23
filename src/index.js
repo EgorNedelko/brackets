1ced7d78ab1ba592d23fe8fb8a8b13a6b28af83c
Egor Nedelko <gojiramedia@gmail.com>
2020-10-23 16:33:11 +0300

    feat: add a stack-based solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-    let round = /\)$/
-    let square = /\]$/
-    let curly = /\}$/
-    let straight = /\|$/
+    var testStr = str //str that will iteratively be reduced and compared to input 
 
-    let beg = str.slice(0, 1)
+    if (str.length % 2 != 0) return false //check for odd-length input
 
-    switch (beg) {
-        case '(':
-            return str.endsWith(')') ? true : false
-        case '[':
-            return str.endsWith(']') ? true : false
-        case '{':
-            return str.endsWith('}') ? true : false
-        case '|':
-            return str.endsWith('|') ? true : false
+    for (let i = 0; i < bracketsConfig.length; i++) { //loop through the samples array
+        for (let j = 0; j < str.length; j++) { //loop through the input str
+            if (str[j] == bracketsConfig[i][0] && str[j+1] == bracketsConfig[i][1]) { //check for adjacent pairs 
+                testStr = str.replace(bracketsConfig[i][0]+bracketsConfig[i][1], '') //redefine testString as the same string but without the adjacent pair
+            }
+        }
     }
-    //return
+//if nothing's been done this iteration ? (yes) if test is empty : (no) recurse with altered testString
+    return testStr == str ? testStr == '' : check(testStr, bracketsConfig)
 }
